57b0dda7e49a68e92173f0e4cb6e0589e0765b61
Vince Laquindanum <laq.vince@gmail.com>
2021-06-24 20:29:52 -0700

    Increases board size

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let grid = document.querySelector(".grid");
 
 // Function to create grid
 function createBoard(){ 
-    for (let i=0; i<100; i++){
+    for (let i=0; i<3200; i++){
     let div = document.createElement("div") 
     grid.appendChild(div) 
     }
