1d26581c49e16ab101c201f9409ad7614cbd8bc2
Vince Laquindanum <laq.vince@gmail.com>
2021-06-25 10:04:57 -0700

    Finally, gets player to move

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Document elements used
 let grid = document.querySelector(".grid");
-let squares = document.querySelectorAll(".grid div");
 // Identify game state variable
 let currentPlayerOne = [];
-let direction = 0;
+let direction;
+let interval;
 // Function to create grid
 function createBoard(){ 
     for (let i=0; i<3200; i++){
@@ -12,29 +12,39 @@ function createBoard(){
     }
 } ;
 // Function to color squares
-function colorSquares() {
+function colorSquares(squares) {
     squares[currentPlayerOne[0]].classList.add("playerOne");
 }
 // Function to start game
 function startGame(){
-    currentPlayerOne = [0];
+    currentPlayerOne.push(0);
     direction = 1;
-    colorSquares();
+    let squares = document.querySelectorAll(".grid div");
+    colorSquares(squares);
     //set interval here when ready
-
+    interval = setInterval(moveOutcome,100);
 }
-
-// Function to move player
-function movePlayer() {
-
-}
-
 // Function for move outcome
 function moveOutcome() {
-
+    let squares = document.querySelectorAll(".grid div");
+    if (checkHits(squares)) {
+       return clearInterval(interval);
+    } else {
+        movePlayer(squares);
+    }
+};
+// Function to move player
+function movePlayer(squares) {
+    currentPlayerOne.unshift(currentPlayerOne[0]+direction);
+    colorSquares(squares);
 }
-
 // Function to check for hits
-
+function checkHits(squares){
+    if (squares[currentPlayerOne[0]+direction].classList.contains("playerOne")) {
+        return true;
+    } else {
+        return false;
+    }
+}
 createBoard();
 startGame();
