9a349f0d14474e0a9f5b7843199d0030dce4c7dc
Vince Laquindanum <laq.vince@gmail.com>
2021-06-28 16:54:00 -0700

    Fixes landing page animation and help popup

--- script-index.js
+++ script-index.js
@@ -43,7 +43,7 @@ $("#menu-item-3").click(function() {
 });
 // Create grid on home page
 function createGrid(){ 
-    for (let i=0; i<7200; i++){
+    for (let i=0; i<4000; i++){
     let div = document.createElement("div");
     grid.appendChild(div); 
     }
@@ -53,12 +53,18 @@ let streamOne = [];
 let streamTwo = [];
 let streamThree = [];
 let streamFour = [];
-let direction = 60;
-let directionTwo = 60;
-let directionThree = 60;
+let streamFive = [];
+let direction = 20;
+let directionTwo = 20;
+let directionThree = 20;
+let streamOneOffset = (20*9);
+let streamTwoOffset = 5+(20*18);
+let streamThreeOffset = 10+(20*9);
+let streamFourOffset = 15+(20);
+let streamFiveOffset = 7+(20*36);
 let interval;
 //Identify constants
-let yDirection = 60;
+let yDirection = 20;
 let xDirection = 1;
 // Function to color squares
 function colorSquares(squares) {
@@ -66,36 +72,39 @@ function colorSquares(squares) {
     squares[streamTwo[0]].classList.add("streamTwo");
     squares[streamThree[0]].classList.add("streamThree");
     squares[streamFour[0]].classList.add("streamFour");
+    squares[streamFive[0]].classList.add("streamFive");
 };
 // Function to start stream
 function startStream() {
-    streamOne.push(380);
-    streamTwo.push(745);
-    streamThree.push(390);
-    streamFour.push(35);
+    streamOne.push(streamOneOffset);
+    streamTwo.push(streamTwoOffset);
+    streamThree.push(streamThreeOffset);
+    streamFour.push(streamFourOffset);
+    streamFive.push(streamFiveOffset);
     let squares = document.querySelectorAll(".grid div");
     colorSquares(squares);
-    setTimeout(function(){ interval = setInterval(moveStream,110); }, 3500);
-    setTimeout(function(){ directionTwo = 1; }, 14000);
-    setTimeout(function(){ directionThree = -1; }, 16000);
-    setTimeout(function(){ directionTwo = 0; }, 16000);
-    setTimeout(function(){ directionThree = 60; }, 18000);
-    setTimeout(function(){ directionTwo = 60; }, 1700);
-    setTimeout(function(){ direction = 1; }, 16000);
-    setTimeout(function(){ clearInterval(interval); }, 22000);
+    setTimeout(function(){ interval = setInterval(moveStream,100); }, 2000);
+    // setTimeout(function(){ directionTwo = 1; }, 14000);
+    // setTimeout(function(){ directionThree = -1; }, 16000);
+    // setTimeout(function(){ directionTwo = 0; }, 16000);
+    // setTimeout(function(){ directionThree = 30; }, 18000);
+    // setTimeout(function(){ directionTwo = 30; }, 1700);
+    // setTimeout(function(){ direction = 1; }, 16000);
+    // setTimeout(function(){ clearInterval(interval); }, 22000);
 }
 // Set a function to continue in intervals
 function moveStream() {
     let squares = document.querySelectorAll(".grid div");
-    // if (streamFour[0] > 7200) {
-    //     clearInterval(interval);
-    // } else {
+    if (streamFour[0] > 4050) {
+        clearInterval(interval);
+    } else {
         streamOne.unshift(streamOne[0]+directionThree);
         streamTwo.unshift(streamTwo[0]+direction);
         streamThree.unshift(streamThree[0]+directionTwo);
         streamFour.unshift(streamFour[0]+directionTwo);
+        streamFive.unshift(streamFive[0]+direction);
         colorSquares(squares);
-    //}
+    }
 };
 // On Load execute these functions
 document.addEventListener("DOMContentLoaded", function() {
@@ -123,7 +132,7 @@ function editOverlay(content) {
 };
 function overlayMessage(message){
     if (message === 'help') {
-        return '<div class ="message-title">!!!Help!!!</div><div class="message-large">Welcome to the "GRID"! The objective is to out-survive your opponent in the arena by avoiding crashing into walls. You and your opponent extend your own walls as you move forward. Collect glowing squares to get an extra life, lose all your lives and you face permanent deresolution ... <div class="blinking-cursor"></div></div><br><div class="message-small">Press SPACE to Close</div>'
+        return '<div class ="message-title">!!!Help!!!</div><div class="message-large">Welcome to the "GRID"! The objective is to out-survive your opponent by trapping them with your movement generated walls and collecting white boxes for extra lives. When a player has exhausted all of their lives, they face permanent deresolution. Win and the player lives to battle on ... <div class="blinking-cursor"></div></div><br><div class="message-small">Press SPACE to Close</div>'
     } else {
         return '<div class ="message-title">!!!Disclaimer!!!</div><div class="message-large">Thank you for playing Tron Lightcycle Battle! This game was created by Vincent Laquindanum without the permission or consent of the Disney franchise. All Tron properties are own by Disney and was not created by Vincent Laquindanum. This game is just for fun and for the sole purpose of finding a job. Please, don\'t sue me ... <div class="blinking-cursor"></div></div><br><div class="message-small">Press SPACE to Close</div>'
     }
